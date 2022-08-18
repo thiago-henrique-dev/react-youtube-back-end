f9d951409c78e9813e4af19d6cd2b7e48c2705b7
thiagohenriquedev <thiagohenriquedev@gmail.com>
2022-08-18 20:27:54 -0300

    connected to database mongoose

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
 const app = express()
+dotenv.config()
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("connected to db");
+    }).catch(err=> {
+        throw err;
+    })
+}
+
 
 app.listen(8800, () => {
+    connect()
     console.log("connected!")
 })
